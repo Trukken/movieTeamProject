009d5c287f20587159450880f6e5b1e3a51f5b65
Trukken <tomolya12@gmail.com>
2019-11-06 14:34:14 +0100

    fixed search bar

--- search.php
+++ search.php
@@ -2,10 +2,14 @@
 
 require_once './connect-to.php';
 
+
 $search = filter_var($_POST['search'], FILTER_SANITIZE_STRING);
 
 $sql = "SELECT * FROM movies WHERE name LIKE '$search%'";
 $query = connectTo($sql);
-$row = mysqli_fetch_assoc($query);
-
-echo $row['name'];
+$array = [];
+echo '<ul id="movies-list">';
+while ($res = mysqli_fetch_assoc($query)) {
+    echo '<li onClick="selectMovie(\'' . $res['name'] . '\')">' . $res['name'] . '</li>';
+}
+echo '</ul>';
