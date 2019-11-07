67c0d586bffd253e624c65542b0e587480c62112
Trukken <tomolya12@gmail.com>
2019-11-07 13:56:08 +0100

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,6 +1,48 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-$id = +filter_var($_GET['details'], FILTER_SANITIZE_NUMBER_INT);
-var_dump($id);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php require_once './looks.php';
+    headerLink(); ?>
+    <title>Document</title>
+</head>
 
+<body>
+    <?php navBar(); ?>
 
+    <form action="#" method="POST">
+        <?php
+
+        require_once './connect-to.php';
+
+        $id = +filter_var($_GET['details'], FILTER_SANITIZE_NUMBER_INT);
+
+        $sql = "SELECT * FROM movies WHERE movie_id=$id";
+
+        $query = connectTo($sql);
+        $row = mysqli_fetch_assoc($query);
+        ?>
+        <input type="text" name="name" placeholder="Movie name" value="<?php echo $row['name']; ?>">
+        <input type="text" name="release" placeholder="Movie name" value="<?php echo $row['release_year']; ?>">
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <?php
+    if (isset($_POST['submit']) && !empty($_POST['release']) && !empty($_POST['name'])) {
+        $name = $_POST['name'];
+        $release = $_POST['release'];
+
+        $sql = "UPDATE movies SET name='$name', release_year='$release' WHERE movie_id=$id";
+        var_dump($sql);
+        connectTo($sql);
+        header("Refresh:0");
+    }
+    ?>
+
+    <?php bodyLink(); ?>
+</body>
+
+</html>
