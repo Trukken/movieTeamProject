f6aed240d344c9924a77e72a613f2e6c94cd918a
Alexandru80 <torrinro@yahoo.com>
2019-11-07 14:44:33 +0100

    vhhjvhjv

--- login.php
+++ login.php
@@ -1,22 +1,3 @@
-<<<<<<< Updated upstream
-<form action="" method="POST">
-    <input type="email" name="email" placeholder="Your email">
-    <input type="password" name="password" placeholder="Your password">
-    <input type="submit" name="login" value="Login">
-    <a href="<?php echo 'registration.php'; ?>">Register</a>
-    <a href="<?php echo 'forgotpassword.php'; ?>">Forgotten password</a>
-</form>
-
-<?php
-require_once './connect-to.php';
-
-if (isset($_POST['login'])) {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    if (strpos($_POST['email'], '@') and $_POST['password']) {
-        echo 'user recognised';
-        $query = 'SELECT *
-=======
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -59,7 +40,6 @@ if (isset($_POST['login'])) {
         if (strpos($_POST['email'], '@') and $_POST['password']) {
             echo 'user recognised';
             $query = 'SELECT *
->>>>>>> Stashed changes
                 FROM users
                 WHERE (user_email = "' . $_POST['email'] . '") and
                 (user_password = "' . $_POST['password'] . '")';
