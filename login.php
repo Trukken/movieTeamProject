ae80985e05f8d6fc2df5f61b7140f6955a066d01
Alexandru80 <torrinro@yahoo.com>
2019-11-07 16:29:44 +0100

    htdtyd

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -40,6 +41,27 @@
         if (strpos($_POST['email'], '@') and $_POST['password']) {
             echo 'user recognised';
             $query = 'SELECT *
+=======
+<form action="" method="POST">
+    <input type="email" name="email" placeholder="Your email">
+    <input type="password" name="password" placeholder="Your password">
+    <input type="submit" name="login" value="Login">
+    <a href="<?php echo 'registration.php'; ?>">Register</a>
+    <a href="<?php echo 'forgotpassword.php'; ?>">Forgotten password</a>
+</form>
+
+<?php
+require_once './connect-to.php';
+
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+	$password = $_POST['password'];
+	$sanitizeMail = filter_var($mail, FILTER_SANITIZE_EMAIL);
+    $sanitizeMail = filter_var($mail, FILTER_VALIDATE_EMAIL);
+    if (strpos($_POST['email'], '@') and $_POST['password']) {
+        echo 'user recognised';
+        $query = 'SELECT *
+>>>>>>> Stashed changes
                 FROM users
                 WHERE (user_email = "' . $_POST['email'] . '") and
                 (user_password = "' . $_POST['password'] . '")';
